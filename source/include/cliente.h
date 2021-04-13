e1bb495ed026d6d4522a23f797410ceb30195c6e
Jonathan Guzman <jonguz5@hotmail.com>
2021-04-12 20:12:32 -0600

    agregado cliente

---
+++ source/include/cliente.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string_util.h>
+
+typedef enum tipoPago{
+    efectivo,
+    tarjetaDebito,
+    tarjetaCredito
+}tipoPago;
+
+typedef struct Cliente{
+    String* nombreCliente;
+    String* id;
+    tipoPago metodoPago;
+    String* telefono;
+    String* numeroCuenta;
+}Cliente;
