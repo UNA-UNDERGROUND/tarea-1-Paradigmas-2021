8e798407452d19bb8bff1811e877d5bfb0b3ed6a
Jonathan Guzman <jonguz5@hotmail.com>
2021-04-12 18:34:48 -0600

    ejemplo de uso de deleteString

--- source/src/source.c
+++ source/src/source.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string_util.h>
 
 int main() {
-    printf("hola mundo\n");
+	String* s = createString("Jonathan");
+    printf("hola %s\n", getString(s));
+	deleteString(&s);
+	//podemos borrarlo varias veces, pues ya es nulo
+	deleteString(&s);
+	if (!s) {
+		printf("eliminado el string\n");
+	}
 }
+
