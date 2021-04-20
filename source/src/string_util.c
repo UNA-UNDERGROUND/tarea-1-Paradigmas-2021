e3d441cdc4bad7cee6044cb6bcc2edf6d68bad30
Jonathan Guzman <jonguz5@hotmail.com>
2021-04-19 19:30:35 -0600

    mejoras del String removido el uso de strcpy_s y correcciones en el manejo de memoria

--- source/src/string_util.c
+++ source/src/string_util.c
@@ -35,18 +35,24 @@ void deleteString(String **self) {
 	}
 }
 const char *getString(String *self) {
-	return self && self->lenght ? self->buffer : "";
+	return (self && self->buffer != NULL) && self->lenght ? self->buffer : "";
 }
 size_t getStringLenght(String *self) { return self ? self->lenght : 0; }
 size_t getStringSize(String *self) { return self ? self->size : 0; }
 bool resizeString(String *self, size_t new_size) {
 	if (self) {
 		if (self->size != new_size) {
-			char *newBuffer = malloc(new_size);
+			char *newBuffer = calloc(new_size, sizeof(char));
 			if (newBuffer) {
 				// hay que copiar los valores y liberar la memoria
 				if (self->size) {
-					strcpy_s(newBuffer, new_size, self->buffer);
+					// si la longitud nueva es menor solo copiamos hasta donde
+					// podemos
+
+					size_t longitud = min(strlen(self->buffer) + 1, new_size);
+					memcpy(newBuffer, self->buffer, longitud);
+					// no es portable
+					// strcpy_s(newBuffer, new_size, self->buffer);
 					free(self->buffer);
 				}
 				self->buffer = newBuffer;
@@ -65,13 +71,22 @@ bool resizeString(String *self, size_t new_size) {
 }
 bool setString(String *self, const char *str) {
 	if (self) {
-		size_t new_len = strlen(str);
+		size_t new_len = strlen(str) + 1;
 		// practicamente no hay costo de copia
 		// en un string nuevo ya que es solo reservar
 		if (self->size < new_len && !resizeString(self, new_len + 1)) {
 			return false;
 		}
-		strcpy_s(self->buffer, self->size, str);
+		// en C es importante contar el caracter nulo
+		// y strlen no lo toma en cuenta
+		if (strcmp(str, "")) {
+			memcpy(self->buffer, str, new_len);
+		} else {
+			memset(self->buffer, 0, self->size);
+		}
+
+		// no es portable
+		// strcpy_s(self->buffer, self->size, str);
 		self->lenght = new_len;
 		return true;
 	}
